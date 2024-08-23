f4eb3905c9e8bdc1b8fa12b10bd5bff3975a8276
Slantall <slantallsstuff@gmail.com>
2024-08-23 13:47:17 -0700

    player movement

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import pygame
 from constants import * 
+from circleshape import * 
+from player import * 
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game_loop(screen)
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player1 = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
 
-
-def game_loop(screen):
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        
+        player1.update(dt)
+        player1.draw(screen)
+        
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        
 
 
 
